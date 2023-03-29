fa2d46ded93ff1fbecbdd3e5885f9ddf5a8355fe
Grand-Magus <1147426328@qq.com>
2023-03-30 01:13:42 +0800

    first commit

---
+++ 0_Setting_APT_source_file.sh
@@ -0,0 +1,19 @@
+sudo bash -c 'echo "deb http://mirrors.tuna.tsinghua.edu.cn/ubuntu/ jammy main restricted universe multiverse" > /etc/apt/sources.list'
+sudo apt-get update
+
+sudo apt-get install build-essential    # build-essential packages, include binary utilities, gcc, make, and so on
+sudo apt-get install man                # on-line reference manual
+sudo apt-get install gcc-doc            # on-line reference manual for gcc
+sudo apt-get install gdb                # GNU debugger
+sudo apt-get install git                # revision control system
+sudo apt-get install libreadline-dev    # a library used later
+sudo apt-get install libsdl2-dev        # a library used later
+sudo apt-get install llvm llvm-dev      # llvm project, which contains libraries used later
+# sudo apt-get install llvm-11 llvm-11-dev # only for ubuntu20.04
+
+sudo apt-get install vim
+mv .vimrc ~/.vimrc
+
+# Install zsh
+sudo apt-get install zsh
+chsh -s /bin/zsh
---
+++ 1_install_and_config_oh_my_zsh.sh
@@ -0,0 +1,31 @@
+# Install oh my zsh
+
+# 官方配置安装
+# sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# 【推荐】脚本配置安装
+sudo apt-get install curl
+sudo apt-get install unzip
+# zsh -c "$(curl -fsSL 'https://api.host.mintimate.cn/fileHost/public/download/1P0R')"
+zsh -c "$(AutoInstallZsh.sh)"
+
+# 修改主题：
+# ZSH_THEME="robbyrussell"
+# 改为
+# ZSH_THEME="ys"
+# 粘贴自动转义
+# DISABLE_MAGIC_FUNCTIONS=true
+# 修改插件：
+# plugins=(git)
+# 改为
+# plugins=(git zsh-syntax-highlighting zsh-autosuggestions)
+
+# Install plugin
+
+# zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+# zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+mv .zshrc ~/.zshrc
+source .zshrc
