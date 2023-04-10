349602e9124be843019d252f2f7774463b12eea6
Grand-Magus <1147426328@qq.com>
2023-04-10 20:08:05 +0800

    Update 1_install_and_config_oh_my_zsh.sh

--- 1_install_and_config_oh_my_zsh.sh
+++ 1_install_and_config_oh_my_zsh.sh
@@ -27,5 +27,5 @@ git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:
 # zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
-mv .zshrc ~/.zshrc
+cp .zshrc ~/.zshrc
 source .zshrc
